451bad67654b3b1edc3be99583d1ebafa64201d6
Ataberk Canıtez <aticanitez@hotmail.com>
2021-02-02 15:29:53 +0300

    first files

---
+++ src/main/java/com/ataberkcanitez/enocajrtest/EnocaJrTestApplication.java
@@ -0,0 +1,13 @@
+package com.ataberkcanitez.enocajrtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnocaJrTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnocaJrTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ataberkcanitez/enocajrtest/EnocaJrTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ataberkcanitez.enocajrtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnocaJrTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
